319462bf383be0def4bb5801cf294148f0363348
leeahwi <48510004+leeahwi@users.noreply.github.com>
2022-11-02 23:30:24 +0900

    upload

---
+++ src/main/java/com/springserver/cocktailspringserver/CocktailSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.springserver.cocktailspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailSpringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailSpringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springserver/cocktailspringserver/CocktailSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springserver.cocktailspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailSpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
